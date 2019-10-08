af93d36b8cb7623a4b787a82d6c12f4f539a26f2
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-10-08 02:52:22 -0500

    Initial skeleton

---
+++ lib/Renard/API/Kiwisolver.pm
@@ -0,0 +1,12 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::API::Kiwisolver;
+# ABSTRACT: API for Kiwisolver constraint solver
+
+use Mu;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-API-Kiwisolver/>
+
+=cut
