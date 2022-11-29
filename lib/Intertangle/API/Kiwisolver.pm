bc6621cacf300ecbb51ddd02f99ddf64e3e7d62a
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-11-29 16:21:04 -0500

    Add deprecated class for Intertangle::API::Kiwisolver
    Fixes <https://github.com/PerlGfx/p5-Graphics-Layout-Kiwisolver/issues/16>.

---
+++ lib/Intertangle/API/Kiwisolver.pm
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+package Intertangle::API::Kiwisolver;
+# ABSTRACT: (DEPRECATED) Renamed to Graphics::Layout::Kiwisolver
+
+warnings::warnif('deprecated', 'Intertangle::API::Kiwisolver is deprecated and should no longer be used. Replace with Graphics::Layout::Kiwisolver.');
+
+1;
